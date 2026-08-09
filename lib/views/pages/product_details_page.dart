86e38ecefa69dca41307017e1ad0a9a1439fa0f9
Administrator <b.abualrub3@student.aaup.edu>
2026-08-09 19:42:56 +0300

    feat: use the cubit in the product details page.

--- lib/views/pages/product_details_page.dart
+++ lib/views/pages/product_details_page.dart
@@ -1,21 +1,42 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_ecommerce_app/view_models/product_details_cubit/product_details_cubit.dart';
 
 class ProductDetailsPage extends StatelessWidget {
-  final String productId;
-
-  const ProductDetailsPage({super.key, required this.productId});
+  const ProductDetailsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Product Details'),
-        actions: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.favorite_border)),
-        ],
-      ),
-      body: Stack(children: [CachedNetworkImage(imageUrl: 'productItem.imgUrl')]),
+    return BlocBuilder(
+      bloc: BlocProvider.of<ProductDetailsCubit>(context),
+      builder: (BuildContext context, state) {
+        if (state is ProductDetailsLoading) {
+          return Scaffold(body: Center(child: CircularProgressIndicator()));
+        } else if (state is ProductDetailsLoaded) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(state.product.name),
+              actions: [
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.favorite_border),
+                ),
+              ],
+            ),
+            body: Stack(
+              children: [CachedNetworkImage(imageUrl: state.product.imgUrl)],
+            ),
+          );
+        } else if (state is ProductDetailsError) {
+          return Scaffold(
+            appBar: AppBar(title: const Text('Error')),
+            body: Center(child: Text(state.message)),
+          );
+        } else {
+          return Scaffold(body: Center(child: Text('Unknown state')));
+        }
+      },
     );
   }
 }
