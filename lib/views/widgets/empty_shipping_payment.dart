31245d7661c41fd21be62c18a481c3da21401c54
Administrator <b.abualrub3@student.aaup.edu>
2026-08-17 17:59:58 +0300

    remove imports

--- lib/views/widgets/empty_shipping_payment.dart
+++ lib/views/widgets/empty_shipping_payment.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_ecommerce_app/utils/app_colors.dart';
-import 'package:flutter_ecommerce_app/utils/app_routes.dart';
-import 'package:flutter_ecommerce_app/view_models/checkout_cubit/checkout_cubit.dart';
 
 class EmptyShippingAndPayment extends StatelessWidget {
   final String title;
   final VoidCallback onTab;
 
-  const EmptyShippingAndPayment({super.key, required this.title, required this.onTab});
+  const EmptyShippingAndPayment({
+    super.key,
+    required this.title,
+    required this.onTab,
+  });
 
   @override
   Widget build(BuildContext context) {
